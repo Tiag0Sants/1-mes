0266f54b0f83c9f24471c933f01d6830f4184693
Tiag0Sants <mbetani.betania.10@gmail.com>
2025-01-13 14:20:37 -0300

    vamos ver se agr vai

--- script.js
+++ script.js
@@ -14,10 +14,6 @@ function createHearts() {
         const duration = Math.random() * 4 + 3; // Duração entre 3s e 7s
         heart.style.animationDuration = `${duration}s`;
 
-        // Adicionando variações no deslocamento horizontal
-        const translateX = (Math.random() - 0.5) * 50; // Movimento horizontal aleatório
-        heart.style.setProperty('--translate-x', `${translateX}vw`);
-
         // Configurando cores aleatórias
         heart.style.backgroundColor = getRandomColor();
 
